7f969cc9243f83c4b7c90911118e19d94bdd260b
Haysten Dcosta <haystendc@gmail.com>
2024-06-06 16:33:21 +0530

    created footer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-(() =>{
- 
+(() => {
+
   const openNavMenu = document.querySelector(".open-nav-menu"),
-  closeNavMenu = document.querySelector(".close-nav-menu"),
-  navMenu = document.querySelector(".nav-menu"),
-  menuOverlay = document.querySelector(".menu-overlay"),
-  mediaSize = 1400;
+    closeNavMenu = document.querySelector(".close-nav-menu"),
+    navMenu = document.querySelector(".nav-menu"),
+    menuOverlay = document.querySelector(".menu-overlay"),
+    mediaSize = 1400;
 
   openNavMenu.addEventListener("click", toggleNav);
   closeNavMenu.addEventListener("click", toggleNav);
@@ -12,54 +12,54 @@
   menuOverlay.addEventListener("click", toggleNav);
 
   function toggleNav() {
-  	navMenu.classList.toggle("open");
-  	menuOverlay.classList.toggle("active");
-  	document.body.classList.toggle("hidden-scrolling");
+    navMenu.classList.toggle("open");
+    menuOverlay.classList.toggle("active");
+    document.body.classList.toggle("hidden-scrolling");
   }
 
-  navMenu.addEventListener("click", (event) =>{
-      if(event.target.hasAttribute("data-toggle") && 
-      	window.innerWidth <= mediaSize){
-      	// prevent default anchor click behavior
-      	event.preventDefault();
-      	const menuItemHasChildren = event.target.parentElement;
-        // if menuItemHasChildren is already expanded, collapse it
-        if(menuItemHasChildren.classList.contains("active")){
-        	collapseSubMenu();
-        }
-        else{
-          // collapse existing expanded menuItemHasChildren
-          if(navMenu.querySelector(".dropdown-menu-branch.active")){
-        	collapseSubMenu();
-          }
-          // expand new menuItemHasChildren
-          menuItemHasChildren.classList.add("active");
-          const subMenu = menuItemHasChildren.querySelector(".sub-menu");
-          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-        }
+  navMenu.addEventListener("click", (event) => {
+    if (event.target.hasAttribute("data-toggle") &&
+      window.innerWidth <= mediaSize) {
+      // prevent default anchor click behavior
+      event.preventDefault();
+      const menuItemHasChildren = event.target.parentElement;
+      // if menuItemHasChildren is already expanded, collapse it
+      if (menuItemHasChildren.classList.contains("active")) {
+        collapseSubMenu();
       }
+      else {
+        // collapse existing expanded menuItemHasChildren
+        if (navMenu.querySelector(".dropdown-menu-branch.active")) {
+          collapseSubMenu();
+        }
+        // expand new menuItemHasChildren
+        menuItemHasChildren.classList.add("active");
+        const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+      }
+    }
   });
-  function collapseSubMenu(){
-  	navMenu.querySelector(".dropdown-menu-branch.active .sub-menu")
-  	.removeAttribute("style");
-  	navMenu.querySelector(".dropdown-menu-branch.active")
-  	.classList.remove("active");
+  function collapseSubMenu() {
+    navMenu.querySelector(".dropdown-menu-branch.active .sub-menu")
+      .removeAttribute("style");
+    navMenu.querySelector(".dropdown-menu-branch.active")
+      .classList.remove("active");
   }
-  function resizeFix(){
-  	 // if navMenu is open ,close it
-  	 if(navMenu.classList.contains("open")){
-  	 	toggleNav();
-  	 }
-  	 // if menuItemHasChildren is expanded , collapse it
-  	 if(navMenu.querySelector(".dropdown-menu-branch.active")){
-        	collapseSubMenu();
-     }
+  function resizeFix() {
+    // if navMenu is open ,close it
+    if (navMenu.classList.contains("open")) {
+      toggleNav();
+    }
+    // if menuItemHasChildren is expanded , collapse it
+    if (navMenu.querySelector(".dropdown-menu-branch.active")) {
+      collapseSubMenu();
+    }
   }
 
-  window.addEventListener("resize", function(){
-     if(this.innerWidth > mediaSize){
-     	resizeFix();
-     }
+  window.addEventListener("resize", function () {
+    if (this.innerWidth > mediaSize) {
+      resizeFix();
+    }
   });
 
 })();
